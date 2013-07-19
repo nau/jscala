32807642657a2f8767da3a75d733d08967f794e4
Alexander Nemish <anemish@gmail.com>
2013-07-19 19:03:32 +0300

    Update JScalaExample.scala

--- jscala-examples/src/main/scala/org/jscala/JScalaExample.scala
+++ jscala-examples/src/main/scala/org/jscala/JScalaExample.scala
@@ -2,14 +2,14 @@ package org.jscala
 
 object JScalaExample {
   def domManipulations() {
-    val html = <script>{ javascript {
+    val html = javascript {
       val node = document.getElementById("myList2").lastChild
       document.getElementById("myList1").appendChild(node)
       val buttons = document.getElementsByTagName("link")
       for (idx <- 0 until buttons.length) {
         console.log(buttons.item(idx).attributes)
       }
-    }.asString}</script>
+    }.print
     println(html)
   }
 
@@ -19,12 +19,42 @@ object JScalaExample {
       window.open("https://github.com")
       history.back()
     }
-    println(js.asString)
+    println(js.print)
+  }
+
+  def complexExample() {
+    val js = javascript {
+      window.setTimeout(() => {
+        val r = new RegExp("d.*", "g")
+        def func(i: String) = r.exec(i)
+        val list = document.getElementById("myList2")
+        val map = collection.mutable.Map[String, String]()
+        if (typeof(map) == "string") {
+          for (idx <- 0 until list.attributes.length) {
+            val attr = list.attributes.item(idx).asInstanceOf[Attribute]
+            map(attr.name) = func(attr.textContent).asInstanceOf[String]
+          }
+        } else {
+          val obj = new {
+            val field = 1
+            def func2(i: Int) = "string"
+          }
+          val links = Array("https://github.com/nau/scala")
+          for (link <- links) {
+            include("var raw = 'JavaScript'")
+            console.log(link + obj.func2(obj.field))
+          }
+          window.location.href = links(0).replace("scala", "jscala")
+        }
+      }, 1000)
+    }
+    println(js.print)
   }
 
   def main(args: Array[String]) {
     domManipulations()
     browserStuff()
+    complexExample()
   }
 }
-
+ 
